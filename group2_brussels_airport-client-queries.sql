e94814d242b88c6b8ebea5a694f7ff86795a0460
corentindpt <57626460+corentindpt@users.noreply.github.com>
2019-12-03 10:46:45 +0100

    Updates

--- group2_brussels_airport-client-queries.sql
+++ group2_brussels_airport-client-queries.sql
@@ -9,35 +9,33 @@
 SET SQL_SAFE_UPDATES=0;
 
 SHOW DATABASES ;
-DROP DATABASE brussels_airport;
-CREATE DATABASE IF NOT EXISTS brussels_airport;
 USE brussels_airport;
 
 /*-------------------------------------------------------------------------
-	Know, for a specific flight number, how many seats are reserved
+1.	Know, for a specific flight number, how many seats are reserved
 ----------------------------------------------------------------------------*/
-SELECT b.flight_idFlight as numero_de_vol,COUNT(*) as nombre_de_siege_reservé FROM reserved_seats rs
+SELECT b.flight_idFlight as numero_de_vol,COUNT(*) as nombre_de_sieges_reservés FROM reserved_seats rs
 	JOIN bookings b ON rs.reservation_idReservation=b.id_bookings
     GROUP BY b.flight_idFlight;
     
  /*-------------------------------------------------------------------------   
-	 	Know, for a specific flight number, the total weight of the luggage
+2.	 Know, for a specific flight number, the total weight of the luggage
 ----------------------------------------------------------------------------*/
 -- SELECT flight_idFlight FROM bookings;
 -- SELECT SUM(luggage_weight) FROM bookings;
-SELECT b.flight_idFlight as numero_de_vol,SUM(luggage_weight) as poids_total_baggage FROM bookings b
+SELECT b.flight_idFlight as numero_de_vol,SUM(luggage_weight) as poids_total_baggage_kg FROM bookings b
     GROUP BY b.flight_idFlight;
     
 /*-------------------------------------------------------------------------   
-	List the passengers who takes a specific flight
+3.	List the passengers who takes a specific flight
 ----------------------------------------------------------------------------*/ 
 SELECT b.flight_idFlight,c.first_name, c.last_name,c.email,c.phone_number,c.birthdate FROM bookings b
 	JOIN clients c ON c.id_client=b.id_bookings
-    WHERE b.flight_idFlight=1
+    WHERE b.flight_idFlight=2
     ORDER BY b.flight_idFlight;
 
 /*-------------------------------------------------------------------------   
-	List all flights that land in a certain destination
+4.	List all flights that land in a certain destination
 ----------------------------------------------------------------------------*/ 
 SELECT d.city as destination, f.id_flight as numero_de_vol,d.flight_time as durée_du_vol,d.distance as distance_km FROM flights f
 	JOIN destination d ON d.id_destination=f.destination_idDestination
@@ -45,41 +43,40 @@ SELECT d.city as destination, f.id_flight as numero_de_vol,d.flight_time as dur�
 	ORDER BY f.id_flight;
     
 /*-------------------------------------------------------------------------   
-	Add at any time a new plane, a new flight or a new destination
+5.	Add at any time a new plane, a new flight or a new destination
 ----------------------------------------------------------------------------*/ 
--- Insert new flights  
+-- 5.1 Insert new plane
+INSERT INTO plane(number_seat)
+	VALUES('40');
+    
+-- 5.2 Insert new flights  
 INSERT INTO flights(take_off_time)
 	VALUES('2200');
 
--- Insert new plane
-INSERT INTO plane(number_seat)
-	VALUES('40');
-
--- Insert new destination
+-- 5.3 Insert new destination
 INSERT INTO destination (city,flight_time,distance)
 	VALUES ('tokyo','230','350');
     
 -- Update flights
 UPDATE flights
-	SET plane_idPlane='5',destination_idDestination='5'
+	SET plane_idPlane='5',destination_idDestination='4'
 	WHERE id_flight='8';
 /*-------------------------------------------------------------------------   
-	Be able to add new client information (e.g. address)
+5.	Be able to add new client information (e.g. address)
 ----------------------------------------------------------------------------*/   
 ALTER TABLE clients ADD COLUMN address VARCHAR(200);
 -- ALTER TABLE clients DROP COLUMN adress; 			/* misspelling therefore deletion of the column*/
 
 /*-------------------------------------------------------------------------   
-	Offer the possibility for our passengers to reserve a flight
+6.	Offer the possibility for our passengers to reserve a flight
 ----------------------------------------------------------------------------*/ 
--- Add Number flight,luggage quantity, luggage weight 
+-- a. Add Number flight,luggage quantity, luggage weight 
 INSERT INTO bookings(luggage_quantity,luggage_weight,flight_idFlight,clients_idClient,id_bookings)
 		VALUES(3,50,2,40,41);
--- Add seats (numb & letter)
+-- b. Add seats (numb & letter)
+INSERT INTO reserved_seats(reservation_idReservation,seat_idSeat)
+		VALUES(41,30);
 
--- Add plane number        
-        
-        
 /*DELETE FROM bookings
 	where id_bookings=43;*/
 /*____________________________SELECT * FROM________________________________*/
